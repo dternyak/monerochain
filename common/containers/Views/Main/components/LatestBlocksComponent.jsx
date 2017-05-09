cc39d3bd5e9eb78473298a630f84d7a00a0b0b6a
Daniel Ternyak <dternyak@gmail.com>
2017-05-09 02:22:04 -0500

    implement paging for latest blocks

--- common/containers/Views/Main/components/LatestBlocksComponent.jsx
+++ common/containers/Views/Main/components/LatestBlocksComponent.jsx
@@ -2,15 +2,11 @@ import React, {Component} from "react";
 import {
     buildColumnFromObject,
     formatBytes,
-    makeCoinbaseData,
-    makeDataWithExcludedPropertiesAndFilterBasedOnProperties,
-    makeTransactionData,
-    removeObjectProperties,
-    removeObjectPropertiesFromListOfObjects
+    makeDataWithExcludedPropertiesAndFilterBasedOnProperties
 } from "utils/tableHelpers";
 import {Link} from "react-router";
-
-
+import {pageToBlock} from "api/api";
+import ActivityIndicator from "react-activity-indicator";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
@@ -19,14 +15,19 @@ export default class LatestBlocksComponent extends Component {
         super(props)
         this.state = {
             latestBlocks: [],
-            data: []
+            data: [],
+            loading: true,
+            currentPage: 0,
+            totalPages: -1
         }
     }
 
-    componentWillMount() {
-        let nextState = this.state;
+    handlePageResults(data) {
         const excludedFields = ["coinbase", "timestamp"]
-        nextState.latestBlocks = this.props.latestBlocks;
+        let nextState = this.state;
+        console.log(data)
+        nextState.latestBlocks = data.data.data.blocks
+        nextState.totalPages = data.data.data.total_page_no;
         const customRender = {
             height: {
                 render: (row) => {
@@ -77,24 +78,56 @@ export default class LatestBlocksComponent extends Component {
         }
         nextState.columns = buildColumnFromObject(nextState.latestBlocks[0], excludedFields, customRender)
         nextState.data = makeDataWithExcludedPropertiesAndFilterBasedOnProperties(nextState.latestBlocks, excludedFields)
+        nextState.loading = false;
+        nextState.currentPage = data.data.data.page
+        nextState.tableLoading = false
         this.setState(nextState)
+
+    }
+
+    componentWillMount() {
+        pageToBlock()
+            .then((data) => this.handlePageResults(data));
     }
 
 
     render() {
         return (
             <div className="column">
-                {
+                { !this.state.loading ?
                     <div>
                         <h5 style={{marginBottom: '2px'}}><b>Latest Blocks ({this.state.data.length})</b></h5>
                         <ReactTable
                             className='-striped -highlight'
-                            showPagination={false}
+                            showPagination={true}
+                            showPageSizeOptions={false}
+                            pages={this.state.totalPages}
                             data={this.state.data}
                             columns={this.state.columns}
                             pageSize={this.state.data.length}
+                            page={this.state.currentPage}
+                            manual
+                            loading={this.state.tableLoading}
+                            onPageChange={(nextPage) => {
+                                // show the loading overlay
+                                this.setState({tableLoading: true})
+                                // fetch your data
+                                pageToBlock(nextPage)
+                                    .then((data) => this.handlePageResults(data))
+                            }
+                            }
                         />
                     </div>
+                    : <div style={{marginTop: '200px'}}>
+                        <ActivityIndicator
+                            number={5}
+                            diameter={70}
+                            duration={150}
+                            activeColor="orange"
+                            borderColor="white"
+                            borderWidth={10}
+                            borderRadius="80%"
+                        /></div>
                 }
             </div>
         )
