b458f98f9a716e980f02e69506c7bd4e3a247ee6
Daniel Ternyak <dternyak@gmail.com>
2017-05-09 02:20:48 -0500

    add pageToBlock api endpoint

--- common/api/api.js
+++ common/api/api.js
@@ -12,6 +12,11 @@ export function searchBlockchain(searchString) {
   return axios.get(`http://139.162.32.245:8081/api/search/${searchString}`);
 }
 
+export function pageToBlock(block=0) {
+  return axios.get(`http://139.162.32.245:8081/api/transactions?page=${block}&limit=25`)
+}
+
+
 export function all(requests) {
   return axios.all(requests)
 }
