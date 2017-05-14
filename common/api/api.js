1eb667e697cb719862ce5398982cdfdf72cb081d
Daniel Ternyak <dternyak@gmail.com>
2017-05-13 22:53:43 -0500

    add getNodeOptions and getNode, have axios use getNode

--- common/api/api.js
+++ common/api/api.js
@@ -1,24 +1,55 @@
 import axios from "axios";
+import store from "store2";
+
+
+export function getNodeOptions() {
+    const nodeSettings = store.get('nodeSettings')
+    if (!nodeSettings) {
+        return null
+    } else {
+        const nodeOptions = nodeSettings.nodeOptions
+        if (!nodeOptions) {
+            return null
+        } else {
+            return nodeOptions
+        }
+    }
+}
+
+export function getNode() {
+    const nodeSettings = store.get('nodeSettings')
+    if (!nodeSettings) {
+        return null
+    } else {
+        const selectedNode = nodeSettings.selectedNode
+        if (!selectedNode) {
+            return null
+        } else {
+            return selectedNode
+        }
+    }
+}
+
+export function getNetworkInfo(origin) {
+    return axios.get(`${origin || getNode()}/api/networkinfo`);
+}
 
 export function getMemPool(limit = 10) {
-    return axios.get(`http://139.162.32.245:8081/api/mempool?limit${limit}`);
+    return axios.get(`${getNode()}/api/mempool?limit=${limit}`);
 }
 
 export function getLatestBlocks() {
-    return axios.get('http://139.162.32.245:8081/api/transactions');
+    return axios.get(`${getNode()}/api/transactions`);
 }
 
 export function searchBlockchain(searchString) {
-    return axios.get(`http://139.162.32.245:8081/api/search/${searchString}`);
+    return axios.get(`${getNode()}/api/search/${searchString}`);
 }
 
 export function pageToBlock(block = 0) {
-    return axios.get(`http://139.162.32.245:8081/api/transactions?page=${block}&limit=25`)
+    return axios.get(`${getNode()}/api/transactions?page=${block}&limit=25`)
 }
-
 
 export function all(requests) {
     return axios.all(requests)
 }
-
-
